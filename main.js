e8f96ed55f668a115bc9663a3d7fde19d808e1a6
Nicolas <nicolasm15@hotmail.com>
2020-05-24 21:22:57 -0300

    feat: Re-write all code with OO changes, try and catch better

--- main.js
+++ main.js
@@ -1,71 +1,121 @@
-var ulElement = document.querySelector('ol');
-var spaceElement = document.querySelector('.log')
-var buttonElement = document.querySelector('button');
+const ulElement = document.querySelector('ol');
+const logElement = document.querySelector('.log')
+const buttonElement = document.querySelector('button');
+const imageElement = document.querySelector('.profile img');
+const nameElement = document.querySelector('.profile p');
 
+const utils = {
+    createListElement(repoName, link) {
+        const listElement = document.createElement('li');
+        const linkElement = document.createElement('a');
+        const textElement = document.createTextNode(repoName);
 
-function createList(params) {
+        linkElement.setAttribute('href', `${link}`);
+        linkElement.setAttribute('target', '_blank');
 
-    var listElement = document.createElement('li');
-    var textElement = document.createTextNode(params);
-
-    listElement.appendChild(textElement);
-    ulElement.appendChild(listElement);
-}
-
-function renderLoading() {
-    ulElement.innerHTML = '';
-    spaceElement.innerHTML = '';
-
-    var loadingDiv = document.createElement('div');
-    loadingDiv.setAttribute('class','loading');
-
-    var loadingText = document.createTextNode("Carregando...");
+        linkElement.appendChild(textElement);
+        listElement.appendChild(linkElement);
+        ulElement.appendChild(listElement);
+    },
+    clearElements() {
+        ulElement.innerHTML = '';
+        logElement.innerHTML = '';
+    },
+    renderLoading() {
+        this.clearElements();
     
-    loadingDiv.appendChild(loadingText);
-    spaceElement.appendChild(loadingDiv);
-}
-
-function renderError(param) {
-    ulElement.innerHTML = "";
-    spaceElement.innerHTML = '';
+        const loadingDiv = document.createElement('div');
+        loadingDiv.setAttribute('class','loading');
     
-    var textError = document.createTextNode(param);
-    var divElement = document.createElement('div');
-    divElement.setAttribute('class', 'logErros');
-
-    divElement.appendChild(textError);
-    spaceElement.appendChild(divElement);
+        const loadingText = document.createTextNode("Loading...");
+        
+        loadingDiv.appendChild(loadingText);
+        logElement.appendChild(loadingDiv);
+    },
+    renderError(param) {
+        this.clearElements();
+        
+        const textError = document.createTextNode(param);
+        const divElement = document.createElement('div');
+        divElement.setAttribute('class', 'logErros');
+    
+        divElement.appendChild(textError);
+        logElement.appendChild(divElement);
+    },
 }
+const dev = {
+    userExists(user) {
+        axios.get(`https://api.github.com/users/${user}`)    
+        .then(function(response) {
+            return true;
+        })
+        .catch(function(error) {
+            console.log(error);
+            if(error.response.data.message === 'Not Found'){
+                utils.renderError('User not found');
+            }else {
+                utils.renderError('error in promise');
+            }
+            return false;
+        });
+    },
+    listGenerate(user){
+        axios.get(`https://api.github.com/users/${user}/repos`)
+        .then(function(response) {
+                utils.clearElements();
+
+                const repos = response.data;
+
+                if(Array.isArray(repos) && repos.length){
+                    for(repo of repos){
+                        const link = repo.html_url;
+                        utils.createListElement(repo.name, link);
+                    }
+                }else{
+                    utils.renderError('User without repositories :(');
+                }
+        });
+    },
+    updateDev(user){
+        axios.get(`https://api.github.com/users/${user}`)
+        .then(function(response) {
+            let name = '';
+            if(response.data.name == null){
+                name = document.getElementById('input').value;
+            }else{
+               name = response.data.name;
+            }
+            imageElement.src = response.data.avatar_url;
+            nameElement.innerHTML = name;
+        });
+    }
+}
+
 
 function showRepositories(){
-    ulElement.innerHTML = '';
-    spaceElement.innerHTML = '';
+    utils.clearElements();
 
-    var user = document.getElementById('input').value;   
-    
-    if (!user) return;
+    const user = document.getElementById('input').value;   
 
-    renderLoading();
-    
-    axios.get(`https://api.github.com/users/${user}/repos`)    
-    .then(function(response) {
-        ulElement.innerHTML = '';
-        spaceElement.innerHTML = '';
-        
-        var repos = response.data;
-        for(repo of repos){
-            createList(repo.name);
-        }
-    })
-    .catch(function(error) {
-        console.log(error)
-        if(error.response.data.message === 'Not Found'){
-            var strErro = 'Usuário não encontrado';
-            renderError(strErro);
-        }else {
-            renderError(error);
-        }
-    });   
+    if (!user) return; // Ignora quando não se digita nada
+
+    utils.renderLoading();
+
+    if(!dev.userExists(user)){
+        dev.listGenerate(user);
+        dev.updateDev(user);
+    }
 }
 
-buttonElement.onclick = showRepositories;
+
+let input = document.querySelector('#input');
+
+buttonElement.addEventListener("click", function(event){
+    buttonElement.onclick = showRepositories();
+});
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    buttonElement.onclick = showRepositories();
+  }
+});
